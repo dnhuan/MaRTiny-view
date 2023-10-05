c0a46f7b1e503f3e9117dae3699411f82be98058
Huan Doan <huandngoc@gmail.com>
2023-10-04 23:40:42 -0700

    Added favicon and changed paddings

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
   return (
     <div className="flex flex-col m-6 p-6">
       <select
-        className="mx-auto my-6 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
+        className="mx-auto my-6 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-1 py-1 inline-flex items-center"
         onChange={(e) => {
           setId(e.target.value);
         }}
